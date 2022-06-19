6def0c7e8ccf21f767f49cf97a32f1c94132ba43
Constantine-M <mez365@mail.ru>
2022-06-19 18:54:03 +0300

    2.2. Модель данных - Post. [#285211 #312407] - fix #1

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -10,25 +10,72 @@ import java.util.Objects;
  */
 public class Post {
 
-    private final int id;
+    private int id;
 
     /** Название вакансии. */
-    private final String title;
+    private String title;
 
     /** Ссылка на описание вакансии. */
-    private final String link;
+    private String link;
 
     /** Описание вакансии. */
-    private final String description;
+    private String description;
 
     /** Дата создания вакансии. */
-    private final LocalDateTime created;
+    private LocalDateTime created;
 
     public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
         this.title = title;
         this.link = link;
         this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(LocalDateTime created) {
         this.created = created;
     }
 
